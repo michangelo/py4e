241e164d4e8901e72f87e2841e91c24d2962f96d
michangelo <112232442+michangelo@users.noreply.github.com>
2023-08-15 21:49:21 +0000

    Aug 15, 2023, 11:49 PM

--- Exercises/scratch.py
+++ Exercises/scratch.py
@@ -91,6 +91,39 @@ def exercise_3_1():
     
 # exercise_3_1()
 
+def exercise_3_3():
+    """Write a program to prompt for a score between 0.0 and 1.0.
+    If the score is out of range, print an error. 
+    If the score is between 0.0 and 1.0, print a grade using the following table"""
+
+    """>= 0.9     A
+       >= 0.8     B
+       >= 0.7     C
+       >= 0.6     D
+       < 0.6      F"""
+
+    # prompt to enter score 
+    score = float(input("Enter Score: "))
+
+    # if score isn't between 0.0 and 1.0, print error message and exit.
+    if score < 0.0 or score > 1.0:
+        print("Error: Please try again.")
+        exit()
+    # assign grade letters 
+    elif score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    elif score < 0.6:
+        print("F")
+
+# exercise_3_3() 
+
+
 
     
     
