849fc86bdfdc0606ed2c1b0a030639f1bfbc20db
michangelo <112232442+michangelo@users.noreply.github.com>
2023-08-15 23:08:49 +0000

    Aug 16, 2023, 1:08 AM

--- Exercises/scratch.py
+++ Exercises/scratch.py
@@ -129,15 +129,15 @@ def exercise_4_6():
     def computepay(h,r):
 
         if h > 40:
-            p = h*40 + (h-40)+r*1.5
-            
+            return h*40 + (h-40)+r*1.5
+
         else:
-            p = h*r
+            return h*r
     
     h = float(input("Enter Hours:"))
     r = float(input("Enter rate per hour:")) 
-    p = computepay(h*r)
-    print("Pay", p)
+p = computepay(h*r)
+print("Pay", p)
 
 exercise_4_6()
 
