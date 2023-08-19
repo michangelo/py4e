151467531690d3099aabac5729d1233e98cff22b
michangelo <112232442+michangelo@users.noreply.github.com>
2023-08-19 12:28:29 +0000

    Aug 19, 2023, 2:28 PM

--- Exercises/ex_09_04/ex_09_04.py
+++ Exercises/ex_09_04/ex_09_04.py
@@ -1,10 +1,33 @@
-fname = input('Enter file name: ')
-fhand = open(fname)
-c = dict()
-for line in fhand:
-    if not line.startswith('From '): continue
-    pieces = line.split()
-    email = pieces[1]
-    c[email] = c.get(email, 0) + 1
+# fname = input('Enter file name: ')
+# fhand = open(fname)
+# c = dict()
+# for line in fhand:
+#     if not line.startswith('From '): continue
+#     pieces = line.split()
+#     email = pieces[1]
+#     c[email] = c.get(email, 0) + 1
 
-print(c)
+# print(c)
+
+import string
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    line = line.rstrip()
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+print(counts)
