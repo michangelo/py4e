2094569331f17f790242ab2ac0d373a73f040bcf
michangelo <112232442+michangelo@users.noreply.github.com>
2023-08-19 10:43:08 +0000

    Aug 19, 2023, 12:43 PM

--- Exercises/ex_08_05/ex_08_05.py
+++ Exercises/ex_08_05/ex_08_05.py
@@ -1,9 +1,16 @@
 fname = input("Enter file name: ")
 fhandle = open(fname)
 
-for line in han:
+for line in fhandle:
     line = line.rstrip()
     wds = line.split()
     if wds[0] != "From": continue 
     print(wds[2])
 
+# fhand = open('mbox-short.txt')
+# count = 0
+# for line in fhand:
+#     words = line.split()
+#     # print('Debug:', words)
+#     if len(words) == 0 or words[0] != 'From' : continue
+#     print(words[2])
